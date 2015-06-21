1c443b51d9ca6828f8670bf08624bdaca2d30a50
Brian Deane <bdeanemachine@gmail.com>
2015-06-21 01:23:54 -0400

    neatened code in setup.sh
      as well as some minor edits:
        - source ~./bash_profile instead
          of the nonexistent ~/.bashrc
        - remove annoying comment

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ############################
-# .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 # thanks to http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
 ############################
@@ -12,43 +11,43 @@ git submodule update
 
 ########## Variables
 
-dotfiles=~/dotfiles                    # dotfiles directory
-olddotfiles=~/dotfiles_old             # old dotfiles backup directory
-files="bash_profile vimrc gitconfig bash_aliases git-completion.bash inputrc json-pretty-print/json_pretty_printer.rb tmux.conf" # list of files to copy
+DOTFILES=~/dotfiles                    # dotfiles directory
+OLDDOTFILES=~/dotfiles_old             # old dotfiles backup directory
+FILES="bash_profile vimrc gitconfig bash_aliases git-completion.bash inputrc json-pretty-print/json_pretty_printer.rb tmux.conf" # list of files to copy
 
 ########## Showtime
 
 # create dotfiles_old in homedir
-echo "Creating $olddotfiles for backup of any existing dotfiles in ~"
-mkdir -p $olddotfiles
+echo "Creating $OLDDOTFILES for backup of any existing dotfiles in ~"
+mkdir -p $OLDDOTFILES
 echo "...done"
 echo ""
 
 # change to the dotfiles directory
-echo "Changing to the $dotfiles directory"
-cd $dotfiles
+echo "Changing to the $DOTFILES directory"
+cd $DOTFILES
 echo "...done"
 echo ""
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then move new dotfiles in
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddotfiles."
-    dir=$(dirname $file)
-    base=$(basename $file)
-    mv ~/.$base $olddotfiles/ || echo "~/.$base does not exist."
-    echo "Copying $dir/.$base to home directory."
+for FILE in $FILES; do
+    echo "Moving any existing dotfiles from ~ to $OLDDOTFILES."
+    DIR=$(dirname $FILE)
+    BASE=$(basename $FILE)
+    mv ~/.$BASE $OLDDOTFILES/ || echo "~/.$BASE does not exist."
+    echo "Copying $DIR/.$BASE to home directory."
     echo ""
-    cp -r $dir/$base ~/.$base # might need a different flag to copy directories
+    cp -r $DIR/$BASE ~/.$BASE # might need a different flag to copy directories
 done
 
 # move vagrant stuff over
 echo "Moving vagrant files to ~"
-cp -R $dotfiles/vagrant ~
+cp -R $DOTFILES/vagrant ~
 echo "...done"
 echo ""
 
 echo "Sourcing .bash_profile"
-source ~/.bashrc
+source ~/.bash_profile
 echo "...done"
 echo ""
 
